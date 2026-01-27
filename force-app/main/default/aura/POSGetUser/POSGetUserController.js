db8fbfb4202ccb1af0d98dcd52e958d7adef2354
Chad Lazarra <chad@bricoleurtech.com>
2026-01-27 20:03:58 +0800

    Changes to FLow

--- force-app/main/default/aura/POSGetUser/POSGetUserController.js
+++ force-app/main/default/aura/POSGetUser/POSGetUserController.js
@@ -1,8 +1,9 @@
 ({
 	doInit : function(component, event, helper) {
 		var userId = helper.getCookie('ContactId');
+        var CashDrawer = helper.getCookie('CashDrawer');
         console.log(userId);
         component.set('v.POSUserId', userId);
-        
+        component.set('v.CashDrawerId', CashDrawer);
 	}
 })
